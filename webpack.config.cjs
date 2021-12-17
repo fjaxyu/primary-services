a516210e60b7b55c0e27fb229741e740222d4ef9
Fjaxyu <wbraithwaite@me.com>
2021-12-17 15:12:36 -0700

    testing some webpack configuration changes

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -5,16 +5,9 @@ const CircularDependencyPlugin = require('circular-dependency-plugin');
 //==---=---=---=--=-=--===---====--===---=---=---=--=-=--===---====--=-===---=---=---=--=-=--===---====--=//
 
 
-
-module.exports = {
+let defaultConfig = {
 	entry: path.resolve(__dirname, 'index.ts'),
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		library: 'PrimaryServices',
-		filename: 'index.js',
-		libraryTarget: 'umd',
-		globalObject: 'this'
-	},
+	devtool: 'source-map',
 	mode: 'production',
 	module: {
 		rules: [
@@ -50,7 +43,96 @@ module.exports = {
 		})
 	],
 	externals: {
-		'node-fetch': 'node-fetch',
-		'unfetch': 'unfetch',
+		// 'node-fetch': 'node-fetch',
+		// 'unfetch': 'unfetch',
 	}
+}
+
+
+
+const serverConfig = {
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		library: {
+			type: 'umd'
+		},
+		filename: 'index.js',
+		globalObject: 'this'
+	},
+	...defaultConfig
 };
+
+
+const clientConfig = {
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		library: {
+			name: 'PrimaryServices',
+			type: 'umd'
+		},
+		filename: 'PrimaryServices.js',
+		globalObject: 'this'
+	},
+	...defaultConfig
+};
+
+
+module.exports = [
+	serverConfig,
+	clientConfig
+];
+
+
+
+// module.exports = {
+// 	entry: path.resolve(__dirname, 'index.ts'),
+// 	devtool: 'source-map',
+// 	output: {
+// 		path: path.resolve(__dirname, 'dist'),
+// 		library: {
+// 			name: 'PrimaryServices',
+// 			type: 'umd'
+// 		},
+// 		filename: 'index.js',
+// 		globalObject: 'this',
+// 		clean: true
+// 	},
+// 	mode: 'production',
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.ts?$/,
+// 				include: path.resolve(__dirname, 'src'),
+// 				// exclude: /(node_modules|build|dist|spec\.ts?$)/,
+// 				exclude: [
+// 					/(node_modules|build|dist)/,
+// 					/\.spec\.tsx/
+// 				],
+// 				use: 'ts-loader'
+// 			},
+// 			{
+// 				test: /\.js$/,
+// 				include: path.resolve(__dirname, 'src'),
+// 				exclude: /(node_modules|bower_components|build|dist)/,
+// 				use: {
+// 					loader: 'babel-loader'
+// 				}
+// 			}
+// 		]
+// 	},
+// 	resolve: {
+// 		extensions: ['.ts', '.js']
+// 	},
+// 	plugins: [
+// 		new CircularDependencyPlugin({
+// 			include: /dir/,
+// 			failOnError: true,
+// 			allowAsyncCycles: false,
+// 			cwd: process.cwd()
+// 		})
+// 	],
+// 	externals: {
+// 		'node-fetch': 'node-fetch',
+// 		'unfetch': 'unfetch',
+// 	}
+// };
